8ca330633bfaabcf61f9fb70508aa4b23e234f31
Christy <mianmianguo2020@gmail.com>
2020-06-02 19:59:35 -0400

    update

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  // host: process.env.HOST,
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  // password: process.env.password,
+  password: "mianmianguo",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Burger Hut');
+INSERT INTO burgers (burger_name) VALUES ('Burger Cave');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Backyard Burgers', true);
