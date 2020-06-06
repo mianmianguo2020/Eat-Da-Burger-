b5da4c4b6f4be8386705bd16ed89ae1ff1878a25
Christy <mianmianguo2020@gmail.com>
2020-06-06 10:39:58 -0400

    m

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,9 @@ require('dotenv').config();
 
 var connection = mysql.createConnection({
   host: process.env.HOST,
-  // host: "127.0.0.1",
   port: 3306,
   user: "root",
   password: process.env.password,
-  // password: "mianmianguo",
   database: "burgers_db"
 });
 
