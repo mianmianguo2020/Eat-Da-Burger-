bdcec126a58224be36b95002064a56861527cebf
Christy <mianmianguo2020@gmail.com>
2020-06-07 16:34:59 -0400

    m

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 require('dotenv').config();
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
   else {
   connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 3306,
   user: "root",
   password: process.env.password,
